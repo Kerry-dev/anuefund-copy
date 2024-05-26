28e20e562f0d64d1c6ddb4af611d051b054daff4
Kerry Ciou <kerry.ciou@gmail.com>
2024-05-26 18:44:08 +0800

    feat(component/featured, featured-table): 新增featured, featured-table元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,20 @@ import { BannerComponent } from './components/banner/banner.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { LipperComponent } from './components/lipper/lipper.component';
 import { CombinationComponent } from './components/combination/combination.component';
+import { FeaturedComponent } from './components/featured/featured.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BannerComponent, BlogComponent, LipperComponent, CombinationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BannerComponent,
+    BlogComponent,
+    LipperComponent,
+    CombinationComponent,
+    FeaturedComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
